2323b0bdcf43214bf9dfb07fdd683ceebd35e714
Kris Dandala <kris.dandala@gmail.com>
2015-05-08 17:08:57 -0500

    Init

---
+++ functions.py
@@ -0,0 +1,22 @@
+#input: (x,y,z,axis,theta)
+#output: (x,y,z) rotated
+#theta must be in radians
+#axis must be "x", "y", or "z"
+
+import math
+
+
+def axis_rotate((x,y,z),axis,theta):
+    cos_t = math.cos(theta)
+    sin_t = math.sin(theta)
+    if axis == "x" or axis == "X":
+        #rotate around x axis
+        x_hat = 
+    elif axis == "y" or axis == "Y":
+        #rotate around y axis
+    elif axis == "z" or axis == "Z":
+        #rotate around z axis
+            
+    else:
+        print "Please specify axis"
+        return (None,None,None)
